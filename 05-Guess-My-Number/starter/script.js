c376787acb44c38a7c19e793b343641390359731
ZannaC <zhanna_s1@inbox.lv>
2023-05-13 08:33:52 +0300

    Add querySelector practice_13.05.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,52 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = '13';
+
+console.log(
+  (document.querySelector('.label-score').textContent = '💯 Score: 115')
+);
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 25;
+console.log(document.querySelector('.guess').value);
+
+const number=Math.random()*20 (random numbers from 1 to 20)
+Math.trunc(Math.random() *20) +1; (trunc-only integer, '+1' for including 20, or only 19 will be the last number)
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high! 🤔';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 😥';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low! 🙄';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 😥';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
