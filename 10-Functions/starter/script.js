18be108ce5499700a64c9d03256b02879d8478ec
ZannaC <zhanna_s1@inbox.lv>
2023-06-06 23:28:47 +0300

    Add  Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+const bookings = [];
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  //   ES6;
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LM123');
+createBooking('LM123', 2, 800);
+createBooking('LM123', 2);
+createBooking('LM123', 5);
+createBooking('LM123', undefined, 1000);
