5315c320ef7789ab31756ac201b06f1fb9463233
ZannaC <zhanna_s1@inbox.lv>
2023-06-06 23:07:29 +0300

    Add theory about which data structure to use

---
+++ 09-Data-Structures-Operators/starter/whichDataStructureToUse119.js
@@ -0,0 +1,8 @@
+
+//// simple list of values -> an array or a set
+//// key value pairs       -> an object or a map
+//// ARRAYS 👉use when you need ordered list of values(might contain dublicates) 👉 ...manipulate data
+//// SETS 👉use when you need to work with unique values 👉 ...high performance really important 👉 ...remove duplicates from arrays
+//-----------------------------------------------------------//
+//// OBJECTS 👉 more traditional key/value store 👉easier to write and access values with . and [] 👉 ..when you need to include functions (methods) 👉working with JSON (can convert to map)
+//// MAPS 👉better performance 👉keys can have any data type 👉easy to iterate 👉easy to compute size  👉need to map key to values 👉 need keys that are not strings
