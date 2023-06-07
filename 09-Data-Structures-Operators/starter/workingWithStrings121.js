a0c7dbfd95590ebb21721bcfd2bf713e0d6edd72
ZannaC <zhanna_s1@inbox.lv>
2023-06-07 06:51:18 +0300

    Add a 1part -  Working With Strings

---
+++ 09-Data-Structures-Operators/starter/workingWithStrings121.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r')); // strings are also zero-based
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+console.log(airline.indexOf('Portugal'));
+
+// slice method
+console.log(airline.slice(4)); // Air
+console.log(airline.slice(4, 7)); //7-4=3
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😁');
+  else console.log('You got lucky 😎');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+console.log(typeof new String('jonas'.slice(1)));
